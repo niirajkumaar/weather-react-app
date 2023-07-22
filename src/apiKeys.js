836763245b15807e59ffc965c0f8d8f471a364ca
niirajkumaar <nirajkumar.cse2020@nsec.ac.in>
2023-07-22 23:16:13 +0530

    commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "d0ea3faa303153db3786acc32a1091de",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
